2274eb0b3b6641b964f83d04c1797ed9e2b3eecf
Thomas Weber <karazon@gmail.com>
2022-07-03 20:32:48 +0200

    POM vervollständigt, application.yml, Testgerüst

---
+++ tdd-styles/src/test/java/guru/thomasweber/dwx22/tddstyles/port/rest/CreateTodoControllerIT.java
@@ -0,0 +1,57 @@
+package guru.thomasweber.dwx22.tddstyles.port.rest;
+
+import guru.thomasweber.dwx22.tddstyles.domain.State;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ComponentScan
+@WebMvcTest(controllers = { CreateTodoController.class })
+public class CreateTodoControllerIT {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    void postCreatesNewTodo() throws Exception {
+        // given
+        String name = "Todo Name";
+        LocalDate dueDate = LocalDate.now().plusDays(1);
+        String json = "{"
+                + "\"name\": \"" + name + "\","
+                + "\"dueDate\": \"" + dueDate + "\""
+                + "}";
+        // when
+        this.mockMvc.perform(
+                post("/todo")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+        // then
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id").exists())
+        .andExpect(jsonPath("$.name").value("Todo Name"))
+        .andExpect(jsonPath("$.dueDate").value(dueDate.toString()))
+        .andExpect(jsonPath("$.state").value(State.OPEN.name()));
+    }
+
+
+}
